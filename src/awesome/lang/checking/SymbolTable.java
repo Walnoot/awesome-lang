367c7f15ff74aad8363a079be94879dc6a696011
djekkoo <djekkoo@hotmail.com>
2015-06-17 15:38:42 +0200

    added context-based offset derivation

--- src/awesome/lang/checking/SymbolTable.java
+++ src/awesome/lang/checking/SymbolTable.java
@@ -1,14 +1,19 @@
 package awesome.lang.checking;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import org.antlr.v4.runtime.ParserRuleContext;
 
+import awesome.lang.GrammarParser.AssignStatContext;
+import awesome.lang.GrammarParser.DeclAssignStatContext;
+import awesome.lang.GrammarParser.DeclStatContext;
 import awesome.lang.model.Type;
 
 public class SymbolTable{
 
 	private final ArrayList<Scope> declarations = new ArrayList<Scope>();
+	private final HashMap<ParserRuleContext, Scope> contextmap = new HashMap<ParserRuleContext, Scope>();
 	
 	public SymbolTable() {
 		// outer scope
@@ -38,14 +43,48 @@ public class SymbolTable{
 		return scope;
 		
 	}
-	
+
 	/** Tries to declare a given identifier in the deepest scope level.
 	 * @return <code>true</code> if the identifier was added,
 	 * <code>false</code> if it was already declared in this scope.
 	 */
-	public boolean add(String id, Type type) {
+	public boolean add(DeclAssignStatContext ctx, Type type) {
 		
-		return this.getCurrentScope().add(id, type);
+		boolean success = this.getCurrentScope().add(ctx.ID().getText(), type);
+		if (success) {
+			this.contextmap.put(ctx,  this.getCurrentScope());
+		}
+		
+		return success;
+		
+	}
+	public boolean add(DeclStatContext ctx, Type type) {
+		
+		boolean success = this.getCurrentScope().add(ctx.ID().getText(), type);
+		if (success) {
+			this.contextmap.put(ctx,  this.getCurrentScope());
+		}
+		
+		return success;
+		
+	}
+	
+	/**
+	 * binds an assignment context to the same scope as the variable definition, fails if variable has not yet been defined
+	 * @param ctx
+	 * @return
+	 */
+	public boolean assign(AssignStatContext ctx) {
+		
+		String id = ctx.ID().getText();
+		for (int i = declarations.size()-1; i >= 0; i--) {
+			if (declarations.get(i).containsKey(id)) {
+				this.contextmap.put(ctx, declarations.get(i));
+				return true;
+			}
+		}
+		
+		return false;
 		
 	}
 
@@ -63,7 +102,31 @@ public class SymbolTable{
 		
 		return false;
 	}
-	
+	public boolean contains(ParserRuleContext ctx) {
+		if (this.contextmap.containsKey(ctx) == false)
+			return false;
+		
+		Scope contains = this.contextmap.get(ctx);
+		Scope current  = this.getCurrentScope();
+		do {
+			
+			if (contains.equals(current))
+				return true;
+			
+			current = current.parent;
+			
+		} while (current != null);
+		
+		return false;
+		
+	}
+
+	/**
+	 * 
+	 * @throws IllegalArgumentException If variable is not found
+	 * @param id
+	 * @return
+	 */
 	public Type getType(String id) {
 		
 		for (int i = declarations.size()-1; i >= 0; i--) {
@@ -72,11 +135,62 @@ public class SymbolTable{
 			}
 		}
 		
-		throw new RuntimeException("Variable not found!");
+		throw new IllegalArgumentException("Variable not found!");
 		
+	}
+
+	private Type getType(ParserRuleContext ctx, String id) {
+		
+		if (this.contains(ctx) == false)
+			return null;
+		
+		return this.contextmap.get(ctx).getType(id);
+		
+	}
+	public Type getType(DeclStatContext ctx) {
+		return this.getType((ParserRuleContext) ctx, ctx.ID().getText());
+	}
+	public Type getType(DeclAssignStatContext ctx) {
+		return this.getType((ParserRuleContext) ctx, ctx.ID().getText());
+	}
+	public Type getType(AssignStatContext ctx) {
+		return this.getType((ParserRuleContext) ctx, ctx.ID().getText());
+	}
+
+	/**
+	 * 
+	 * @throws IllegalArgumentException if variable is not found
+	 * @param ctx
+	 * @param id
+	 * @return
+	 */
+	private int getOffset(ParserRuleContext ctx, String id) {
+		
+		if (this.contextmap.containsKey(ctx) == false)
+			throw new IllegalArgumentException("Variable not found!");
+		
+		return this.contextmap.get(ctx).getOffset(id);
+		
+	}
+	public int getOffset(DeclStatContext ctx) {
+		return this.getOffset((ParserRuleContext) ctx, ctx.ID().getText());
+	}
+	public int getOffset(DeclAssignStatContext ctx) {
+		return this.getOffset((ParserRuleContext) ctx, ctx.ID().getText());
+	}
+	public int getOffset(AssignStatContext ctx) {
+		return this.getOffset((ParserRuleContext) ctx, ctx.ID().getText());
 	}
 	
 	public int getOffset(String id){
-		return 0;
+
+		for (int i = declarations.size()-1; i >= 0; i--) {
+			if (declarations.get(i).containsKey(id)) {
+				return declarations.get(i).getOffset(id);
+			}
+		}
+		
+		throw new IllegalArgumentException("Variable not found!");
+		
 	}
 }
